1ce0260ee351b217b38da8aa75318057e5e39c79
RBM22 <rakrbm@gmail.com>
2018-03-04 22:03:36 +0530

    maybe this

--- performance-tracker/Form1.Designer.cs
+++ performance-tracker/Form1.Designer.cs
@@ -54,7 +54,7 @@
             this.Files.BackColor = System.Drawing.Color.Transparent;
             this.Files.Location = new System.Drawing.Point(4, 22);
             this.Files.Name = "Files";
-            this.Files.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Files.Padding = new System.Windows.Forms.Padding(3);
             this.Files.Size = new System.Drawing.Size(835, 337);
             this.Files.TabIndex = 0;
             this.Files.Text = "Files";
@@ -63,7 +63,7 @@
             // 
             this.MasterEntry.Location = new System.Drawing.Point(4, 22);
             this.MasterEntry.Name = "MasterEntry";
-            this.MasterEntry.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.MasterEntry.Padding = new System.Windows.Forms.Padding(3);
             this.MasterEntry.Size = new System.Drawing.Size(835, 337);
             this.MasterEntry.TabIndex = 1;
             this.MasterEntry.Text = "Master Entry";
